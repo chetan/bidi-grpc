71f8f99744110e2b79595ec94d2a664ae4f69849
Chetan Sarva <chetan@pixelcop.net>
2018-01-05 10:13:47 -0500

    initial import

---
+++ yamux_dialer.go
@@ -0,0 +1,38 @@
+package bidigrpc
+
+import (
+	"errors"
+	"net"
+	"sync"
+	"time"
+
+	"github.com/hashicorp/yamux"
+)
+
+// YamuxDialer is a grpc dialer implementation built on yamux
+type YamuxDialer struct {
+	session *yamux.Session
+	mu      *sync.Mutex
+}
+
+// NewYamuxDialer creates a new dialer
+func NewYamuxDialer() *YamuxDialer {
+	return &YamuxDialer{mu: &sync.Mutex{}}
+}
+
+// SetSession updates the session used by the dialer
+func (y *YamuxDialer) SetSession(sess *yamux.Session) {
+	y.mu.Lock()
+	defer y.mu.Unlock()
+	y.session = sess
+}
+
+// Dial a connection using a yamux session
+func (y *YamuxDialer) Dial(addr string, d time.Duration) (net.Conn, error) {
+	y.mu.Lock()
+	defer y.mu.Unlock()
+	if y.session == nil || y.session.IsClosed() {
+		return nil, errors.New("not connected")
+	}
+	return y.session.Open()
+}
